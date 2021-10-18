3ee614656ba3ff746861b278d1f1f7fda3407093
vitorararuna <contato.vitorararuna@gmail.com>
2021-10-18 10:49:39 -0300

    CSR x SSR

---
+++ src/pages/users.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Users({ users }) {
+    // const [users, setUsers] = useState([]);
+
+    // const fetchUsers = async () => {
+    //   const response = await axios.get(
+    //     'https://jsonplaceholder.typicode.com/users'
+    //   );
+    //   const data = await response.data;
+
+    //   setUsers(data);
+    // };
+
+    // console.log(users);
+
+    // useEffect(() => {
+    //   fetchUsers();
+    // }, []);
+
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user.namerr}>
+                    {user.name}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export async function getStaticProps(context) {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    const data = await response.data;
+
+    return {
+        props: { users: data }, ///passando para props do meu componente
+    };
+}
+
+export default Users;
